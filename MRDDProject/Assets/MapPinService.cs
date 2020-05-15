1705722425260265cc0ee9ed232d49a6227ed5b0
Yonet <aysegulyonet@gmail.com>
2020-05-14 20:30:36 -0700

    feat(pin): add click interaction

---
+++ MRDDProject/Assets/MapPinService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Geospatial;
+using Microsoft.Maps.Unity;
+using System;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+public class MapPinService : MonoBehaviour
+{
+    [SerializeField]
+    private MapPinLayer _mapPinLayer = null;
+
+    [SerializeField]
+    private MapPin _mapPinPrefab = null;
+
+    [SerializeField]
+    private TextAsset _mapPinLocationsCsv = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(_mapPinLayer != null);
+        Debug.Assert(_mapPinPrefab != null);
+        Debug.Assert(_mapPinLocationsCsv != null);
+
+        var lines = _mapPinLocationsCsv.text.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        _mapPinPrefab.gameObject.SetActive(false);
+
+        foreach (var csvLine in lines)
+        {
+            var csvEntries = csvLine.Split(',');
+
+            var mapPin = Instantiate(_mapPinPrefab);
+            mapPin.Location = new LatLon(
+                double.Parse(csvEntries[0], NumberStyles.Number, CultureInfo.InvariantCulture),
+                double.Parse(csvEntries[1], NumberStyles.Number, CultureInfo.InvariantCulture)
+            );
+      
+            Debug.Log("_mapPinLayer.LayerName" + _mapPinLayer.LayerName);
+            _mapPinLayer.MapPins.Add(mapPin);
+            mapPin.GetComponentInChildren<TextMeshPro>().text = csvEntries[2].ToLower() == "null" ? "" : csvEntries[2];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- SampleProject/MRTKSamples/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,6 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\SampleProject\MRTKSamples\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\BlobReader.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\SampleProject\MRTKSamples\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\Debugger.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\SampleProject\MRTKSamples\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\SampleProject\MRTKSamples\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\SampleProject\MRTKSamples\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\VmStringUtils.cpp"
