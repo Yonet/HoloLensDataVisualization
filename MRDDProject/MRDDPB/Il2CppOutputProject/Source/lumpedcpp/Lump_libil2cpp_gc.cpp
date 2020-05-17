5f68cbf91efb0bf907a36ec556f05ece1deea91c
Yonet <aysegulyonet@gmail.com>
2020-05-17 09:58:23 -0700

    change gitignore

--- MRDDProject/MRDDPB/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_gc.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\gc\BoehmGC.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GCHandle.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GarbageCollector.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\gc\NullGC.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrier.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrierValidation.cpp"
--- MRDDProject/MRDDPB/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_mono.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\mono\MonoPosixHelper.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\ThreadPoolMonitorThread.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\ThreadPoolWorkerThread.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms-io-poll.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms-io.cpp"
-#include "C:\Users\Aysegul\Documents\Repos\Unity\MapsSDK-Unity-0.7.1\MRDDProject\MRDDPB\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms.cpp"
